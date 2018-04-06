350c376563ee87f167f58acee5b438d9602e0cac
guorui <proheart123@gmail.com>
2018-04-06 11:41:24 -0500

    + add questions

--- src/company/LinkedIn/phoneinterview/ValidateBST.java
+++ src/company/LinkedIn/phoneinterview/ValidateBST.java
@@ -11,6 +11,18 @@ import java.util.Queue;
  * LC 98
  */
 public class ValidateBST {
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) return true;
+        if (root.val >= maxVal || root.val <= minVal) return false;
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+    }
+
+
     class Instruction{
         int code; // 0.visit; 1.print
         TreeNode node;
